978fe270ecaca2ed6eda3c798d114974389e58f7
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2023-04-28 19:29:11 +0200

    add contact page

--- src/js/slick.js
+++ src/js/slick.js
@@ -4,3 +4,23 @@ $('.carousel').slick({
     autoplaySpeed: 3000,
 
 });
+
+
+$(document).ready(function () {
+    $('.opinions__boxes').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        mobileFirst: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }],
+
+    });
+})
